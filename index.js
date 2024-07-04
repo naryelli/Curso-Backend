6af71cf733f58276bf8aba1f639cc5c530244dea
NARYELLI JULIA DE SOUZA HENRIQUE <naryelli_henrique@estudante.sesisenai.org.br>
2024-07-04 17:35:37 -0300

    sla

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require ('express')
+const { Client } = require('pg')
+require('dotenv').config()
+
+const client =  new Client({
+    host: process.env.host,
+    port:process.env.port,
+    user:process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+})
+
 
 const app = express()
+
+const connectDB = async () => {
+    client
+    .connect()
+    .then(() => {
+        console.log('a conexão funcionou');
+    }).catch((err) => {
+        console.error ('erro ao conectar api')
+    });
+};
+connectDB()
 
 app.get('/test-api', function(req, res){
     res.send('nossa api ta funcionando')
