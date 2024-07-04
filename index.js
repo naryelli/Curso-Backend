a7e524aa3f7aef8b888c9d87eee46c5c33bae1bf
NARYELLI JULIA DE SOUZA HENRIQUE <naryelli_henrique@estudante.sesisenai.org.br>
2024-07-04 16:27:43 -0300

    2

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+const express = require ('express')
 
-function filtrarPares(arr){
-  nova_arr = []
-  for(let i=0; i < arr.length; i++){
-    if(arr[i] % 2 === 0){
-        nova_arr.push(arr[i])
-    }
-  }
+const app = express()
+
+app.get('/test-api', function(req, res){
+    res.send('nossa api ta funcionando')
+})
+
+app.listen(8000)
 
 
-    return nova_arr
-}
+// localhost:8000/test-api
 
-arr= [1,2,3,4,5]
-resultado = filtrarPares(arr)
-console.log(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+// function filtrarPares(arr){
+//   nova_arr = []
+//   for(let i=0; i < arr.length; i++){
+//     if(arr[i] % 2 === 0){
+//         nova_arr.push(arr[i])
+//     }
+//   }
+
+
+//     return nova_arr
+// }
+
+// arr= [1,2,3,4,5]
+// resultado = filtrarPares(arr)
+// console.log(resultado)
